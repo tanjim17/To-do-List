f45633dfa12fe2b0bb8f5d3a960a71f3d5b972b9
tanjim17 <tanjim.ak49@gmail.com>
2022-10-28 00:52:03 +0600

    schema, create object and save

---
+++ backend/models/todo.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+}, {timestamps: true});
+
+module.exports = mongoose.model('Todo', todoSchema);
--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Todo = require('../models/todo');
 
 const router = express.Router();
 
@@ -10,10 +11,15 @@ router.get('/:id', (req, res) => {
     res.send({ name: 'get single todo' });
 });
 
-router.post('/', (req, res) => {
-    console.log("aaa");
+router.post('/', async (req, res) => {
     console.log(req.body);
-    res.send({ name: 'Posted' });
+    const {title, description, date} = req.body;
+    try {
+        const todo = await Todo.create({title, description, date});
+        res.status(200).json(todo);
+    } catch (err) {
+        res.status(400).json({error: err.message});
+    }
 });
 
 router.delete('/:id', (req, res) => {
